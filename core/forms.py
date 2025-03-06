8e7b8758266482a6a38cbca799bd40a882ea14a4
geronilton <geronilton001@gmail.com>
2025-03-06 18:17:10 -0300

    estilo dos labels

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,9 @@
-from django.forms import ModelForm
+from django import forms
 from .models import PontosDeColeta, Usuarios
 from django.contrib.auth.forms import UserCreationForm
 
 
-class PontoDeColetaForm(ModelForm):
+class PontoDeColetaForm(forms.ModelForm):
     class Meta:
         model = PontosDeColeta
         fields = ['tipo', 'descricao', 'imagem']
@@ -13,3 +13,31 @@ class UsuariosForm(UserCreationForm):
     class Meta:
         model = Usuarios
         fields = ['username','email','password1', 'password2']
+        labels = {
+            'username': 'Nome de Usuário',
+            'email': 'Endereço de E-mail',
+            'password1': 'Senha',
+            'password2': 'Confirmar Senha',
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite seu nome de usuário',
+                'style': 'width: 80%; padding: 10px; margin-bottom: 10px;'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite seu e-mail',
+                'style': 'width: 80%; padding: 10px; margin-bottom: 10px;'
+            }),
+            'password1': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha',
+                'style': 'width: 100%; padding: 10px; margin-bottom: 10px;'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirme sua senha',
+                'style': 'width: 100%; padding: 10px; margin-bottom: 20px;'
+            }),
+        }
